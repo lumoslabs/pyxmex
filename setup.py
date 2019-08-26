21f1f9ff100b43989c05538eb9797d9fcb7bcc83
tanjabrzak <break.tanja@gmail.com>
2019-08-26 15:35:50 +0200

    add six package

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def do_setup():
         version=version,
         packages=find_packages(),
         zip_safe=False,
-        install_requires=['pyyaml'],
+        install_requires=['pyyaml', 'six==1.12.0'],
         package_data={'': ['pyxmex/config/*.yml']},
         include_package_data=True,
         author='Rob Froetscher, Joyce Lau',
